394691c2edc37e4bac4b04994febf805f1d3a4c5
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-02-22 17:34:02 +0530

    octave file for Assignment1

--- Assignment/Assignment1/ph15m015_1/recurrence.m
+++ Assignment/Assignment1/ph15m015_1/recurrence.m
@@ -6,7 +6,7 @@
 
 N = 15              % Number of terms in the recurrence
 I = zeros(N,1)      % initialize vector of N elements
-I(1) = 0.63662      % initial condition I0 = 0.31831
+I(1) = 0.63662      % initial condition I0 = 0.63662
 
 for n = 1:N
     I(n+1) = (-1/pi) * 1^(2*n) * cos(pi) + (2*n)/(pi^2) * 1^(2*n - 1) * sin(pi) - ((2*n)*(2*n - 1)/(pi^2)) * I(n)
