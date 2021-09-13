9af6cb7b543b4cb8e38ffc8fa0e8fef3d77c8b35
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-09-13 15:12:26 +0530

    script to construct the polynomial interpolation using lagrange polynomial

--- Scripts/Python/Aug_27/polynomial.py
+++ Scripts/Python/Aug_27/polynomial.py
@@ -3,10 +3,15 @@
 # Name: D.Saravanan
 # Date: 27/08/2021
 
-""" Script to construct the polynomial interpolation using Lagrange polynomial """
+""" Script to construct the polynomial interpolation 
+using Lagrange polynomial """
 
 import numpy as np
 import matplotlib
+matplotlib.rcParams['pgf.texsystem'] = 'pdflatex'
+matplotlib.rcParams.update({'font.family': 'serif', 'font.size': 8,
+    'axes.labelsize': 10, 'axes.titlesize': 10, 'figure.titlesize': 10})
+matplotlib.rcParams['text.usetex'] = True
 import matplotlib.pyplot as plt
 matplotlib.use('Agg')
 
@@ -39,11 +44,10 @@ N = 9
 
 xnodes = np.linspace(-1,1,N)
 fnodes = np.sin(np.pi*xnodes)
-#fnodes = 1/(1 + 25 * xnodes**2)
 
+# true function calculated with equi-spaced nodes
 x = np.linspace(-1,1,1000)
 f = np.sin(np.pi*x)
-#f = 1/(1 + 25 * x**2)
 
 fintp = interpolate(fnodes, xnodes, x)
 
@@ -51,10 +55,11 @@ error = max(abs(f - fintp)/abs(f))
 print(error)
 
 fig, ax = plt.subplots()
-ax.plot(x, f, 'r')
-ax.plot(x, fintp, 'g')
-ax.plot(xnodes, fnodes, 'b*')
-ax.set(xlim=(-1, 1), ylim=(-1.2, 1.2))
-ax.set_title(r"Lagrange polynomial interpolation", fontsize=11)
-ax.grid(True)
-plt.savefig('graph.png')
+ax.plot(x, f, c='olive', ls='dotted', label=r'$\sin(\pi x)$')
+ax.plot(x, fintp, c='teal', label=r'$Interpolation$')
+ax.plot(xnodes, fnodes, 'm.', label=r'$N = 9$')
+ax.set(xlim=(-1, 1), ylim=(-1.1, 1.1))
+ax.set(xlabel=r'$x$', ylabel=r'$\sin(\pi x)$')
+ax.set_title(r'Lagrange polynomial interpolation')
+ax.grid(True); ax.legend()
+plt.savefig('polynomial.pdf')
