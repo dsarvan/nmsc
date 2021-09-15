a696d868e38c8a87b6eb1e9f6a4170c33202946d
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-09-15 18:53:40 +0530

    script to construct the polynomial interpolation for runge function using legendre nodes

--- Scripts/Python/Aug_31/rungelegendre.py
+++ Scripts/Python/Aug_31/rungelegendre.py
@@ -7,6 +7,8 @@
 for Runge Function using Legendre nodes """
 
 import numpy as np
+import numpy.polynomial.polynomial as poly
+from scipy.special import legendre
 import matplotlib
 matplotlib.rcParams['pgf.texsystem'] = 'pdflatex'
 matplotlib.rcParams.update({'font.family': 'serif', 'font.size': 8,
@@ -40,9 +42,12 @@ def lagrange(xnodes, x, i):
     return val
 
 
-N = 9
+N = 21
 
-xnodes = np.cos((4*np.linspace(0,N-1,N) - 1)/(4*N + 2) * np.pi)
+Pn = list(legendre(N))
+Pn.reverse()
+
+xnodes = poly.polyroots((Pn))
 fnodes = 1/(1 + 25 * xnodes**2)
 
 # true function calculated with equi-spaced nodes
@@ -58,8 +63,8 @@ fig, ax = plt.subplots()
 ax.plot(x, f, c='olive', ls='dotted', label=r'$\displaystyle\frac{1}{1+25 x^{2}}$')
 ax.plot(x, fintp, c='teal', label=r'$Interpolation$')
 ax.plot(xnodes, fnodes, 'm.', label=r'$N = 21$')
-#ax.set(xlim=(-1, 1), ylim=(0, 1.1))
+ax.set(xlim=(-1, 1), ylim=(0, 1.1))
 ax.set(xlabel=r'$x$', ylabel=r'$\displaystyle\frac{1}{1+25 x^{2}}$')
 ax.set_title(r"Runge Function with Legendre nodes")
 ax.grid(True); ax.legend()
-plt.savefig('rungelegendre.png')
+plt.savefig('rungelegendre.pdf')
