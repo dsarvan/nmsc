9bb725f43cfbe347bc1273d5bdd80e8daad48016
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-09-19 15:20:21 +0530

    script to compute least-squares

--- Scripts/Python/Sep_03/leastsquare.py
+++ Scripts/Python/Sep_03/leastsquare.py
@@ -20,18 +20,33 @@ M = 10
 N = 1000
 
 x = np.linspace(-1, 1, N+1)
+f = 1/(1 + 25 * x**2)
 
 X = np.ones((N+1, M+1))
 
 for k in range(1, M+1):
     X[:, k] = x**k
 
-print(np.linalg.cond(X))
-print(np.linalg.cond(np.dot(X.T,X)))
+cond_X = np.linalg.cond(X)
+cond_XX = np.linalg.cond(X.T @ X)
 
 Q, R = np.linalg.qr(X)
-print(np.linalg.cond(R))
 
-f = 1/(1 + 25 * x**2)
+cond_R = np.linalg.cond(R)
 
+print(cond_X, cond_XX, cond_R)
 
+sol = np.linalg.solve(R, Q.T@f)
+
+f_LS = X @ sol
+
+# check that the solution is correct
+print(np.allclose(R@sol, Q.T@f))
+
+fig, ax = plt.subplots()
+ax.plot(x, f, c='olive', ls='dotted', label=r'$\displaystyle\frac{1}{1+25 x^{2}}$')
+ax.plot(x, f_LS, c='teal', label=r'$Least-Squares$')
+ax.set(xlabel=r'$x$', ylabel=r'$\displaystyle\frac{1}{1+25 x^{2}}$')
+ax.set_title(r'Least-Squares approximation')
+ax.grid(True); ax.legend()
+plt.savefig('leastsquares.png')
