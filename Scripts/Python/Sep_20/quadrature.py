17f2af347e70b568bc98df8291002f937a34f103
Dayalan Saravanan <dayalsaravanan@gmail.com>
2021-10-17 13:30:02 +0530

    script to implement the different quadrature

--- Scripts/Python/Sep_20/quadrature.py
+++ Scripts/Python/Sep_20/quadrature.py
@@ -14,6 +14,7 @@ matplotlib.rcParams['text.usetex'] = True
 import matplotlib.pyplot as plt
 matplotlib.use('Agg')
 
+# end points of interval
 a, b = 0, 1
 
 # function to be integrated
@@ -23,19 +24,45 @@ f = lambda x: np.exp(-x**2)
 g = lambda x: -2*x*np.exp(-x**2)
 
 # exact value of the integral
-exact_integral = 0.746824132812427025399
+exact = 0.746824132812427025399
 
 # number of different set of grids
 Ngrids = 10
 
-h = zeros(Ngrids)   # different grid spacings
+h = np.zeros(Ngrids)       # different grid spacings
 
-rect = zeros(Ngrids)    # rectangular rule
-trap = zeros(Ngrids)    # trapezoidal rule
-simp = zeros(Ngrids)    # simpson rule
-rend = zeros(Ngrids)    # rectangular rule with end point correction
-tend = zeros(Ngrids)    # trapezoidal rule with end point correction
+rect = np.zeros(Ngrids)    # rectangular rule
+trap = np.zeros(Ngrids)    # trapezoidal rule
+simp = np.zeros(Ngrids)    # simpson rule
+rend = np.zeros(Ngrids)    # rectangular rule with end point correction
+tend = np.zeros(Ngrids)    # trapezoidal rule with end point correction
 
-for k in range(1, Ngrids):
-    N = 10*2**(k+1)             # number of grid points
+for k in range(0, Ngrids):
+    N = 10*2**(k+2)             # number of grid points
     h[k] = (b - a)/(N - 1)      # grid spacing 
+    x = np.linspace(a,b,N)      # grid points
+    y = 0.5*(x[0:N-1] + x[1:N]) # mid-points of each panel
+
+    rect[k] = h[k]*np.sum(f(y))                     # rectangular rule
+    trap[k] = h[k]*(np.sum(f(x)) - (f(a) + f(b))/2) # trapezoidal rule 
+    simp[k] = (2*rect[k] + trap[k])/3               # simpson rule
+    rend[k] = rect[k] + h[k]**2 / 24*(g(b) - g(a))  # rectangular rule with end-point correction
+    tend[k] = trap[k] - h[k]**2 / 12*(g(b) - g(a))  # trapezoidal rule with end-point correction
+
+# error calculations
+rect_err = abs(np.double(rect - exact))     # error in rectangular rule
+trap_err = abs(np.double(trap - exact))     # error in trapezoidal rule
+simp_err = abs(np.double(simp - exact))     # error in simpson rule
+rend_err = abs(np.double(rend - exact))     # error in rectangular rule with end-point correction
+tend_err = abs(np.double(tend - exact))     # error in trapezoidal rule with end-point correction
+
+fig, ax = plt.subplots()
+ax.loglog(h, rect_err, 'r.--', label=r'rectangular')
+ax.loglog(h, trap_err, 'b.--', label=r'trapezoidal')
+ax.loglog(h, simp_err, 'k.--', label=r'simpsonrule')
+ax.loglog(h, rend_err, 'r+--', label=r'rectendpont')
+ax.loglog(h, tend_err, 'b+--', label=r'trapendpont')
+ax.set(xlabel=r'grid size', ylabel=r'error in quadrature')
+ax.set_title(r'Quadrature convergence')
+ax.grid(True); ax.legend()
+plt.savefig('quadrature.pdf')
