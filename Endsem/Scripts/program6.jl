d73f146f18c21687741dc4bdd8542f930d5b4612
Dayalan Saravanan <dayalsaravanan@gmail.com>
2022-02-05 22:11:34 +0530

    program implement different quadrature and error behaviour

--- Endsem/Scripts/program6.jl
+++ Endsem/Scripts/program6.jl
@@ -1,13 +1,19 @@
 #!/usr/bin/env julia
+# File: program6.jl
+# Name: D.Saravanan
+# Date: 02/12/2021
 
-# Program to implement the different quadrature and see how the error behaves
-
+""" Program to implement the different quadrature and see how the error behaves """
 
 import PyPlot
 const plt = PyPlot
-
-
+plt.rc("pgf", texsystem="pdflatex")
+plt.rc("font", family="serif", weight="normal", size=8)
+plt.rc("axes", labelsize=10, titlesize=10)
+plt.rc("figure", titlesize=10)
+plt.rc("text", usetex="True")
 using LaTeXStrings
+using LinearAlgebra
 using FastGaussQuadrature
 
 # end points of interval
@@ -16,8 +22,11 @@ a, b = -1, 1
 # function to be integrated
 f = x -> exp.(-x.^2)
 
-# derivative of the function
+# first derivative of the function
 g = x -> -2*x*exp(-x.^2)
+
+# third derivative of the function
+s = x -> 4*x*(3 - 2*x.^2)*exp(-x.^2)
 
 # exact value of the integral
 exact = 1.49364826562485405080
@@ -30,10 +39,10 @@ Ngrids = length(N)
 
 h = zeros(Ngrids)
 
-trap = zeros(Ngrids)
-tend = zeros(Ngrids)
-tent = zeros(Ngrids)
-gleg = zeros(Ngrids)
+trap = zeros(Ngrids)    # trapezoidal rule
+tend = zeros(Ngrids)    # trapezoidal rule using first derivative
+tent = zeros(Ngrids)    # trapezoidal rule using first and third derivative
+gleg = zeros(Ngrids)    # gauss-legendre quadrature rule
 
 for (k, N) in enumerate(N)
     h[k] = (b - a)/(N - 1)
@@ -44,13 +53,24 @@ for (k, N) in enumerate(N)
 
     trap[k] = h[k]*(sum(f(x)) - (f(a) + f(b))/2)
     tend[k] = trap[k] - (h[k]^2 / 12) * (g(b) - g(a))
-    tent[k] = tend[k] + (h[k]^4 / 720) * (g(b) - g(a))
-    gleg[k] = wnode'.*f(xnode)
-
+    tent[k] = tend[k] + (h[k]^4 / 720) * (s(b) - s(a))
+    gleg[k] = dot(wnode, f(xnode))
 end
 
 # error calculations
-trap_error = abs(trap - exact)
-tend_error = abs(tend - exact)
-tent_error = abs(tent - exact)
-gleg_error = abs(gleg - exact)
+trap_err = abs.(Float64.(trap .- exact))
+tend_err = abs.(Float64.(tend .- exact))
+tent_err = abs.(Float64.(tent .- exact))
+gleg_err = abs.(Float64.(gleg .- exact))
+
+println(gleg_err)
+
+fig, ax = plt.subplots()
+ax.loglog(h, trap_err, "b.--", label=raw"trapezoidal rule")
+ax.loglog(h, tend_err, "r.--", label=raw"trapezoidal rule using 1st derivative")
+ax.loglog(h, tent_err, "m.--", label=raw"trapezoidal rule using 1st and 3rd derivative")
+ax.loglog(h, gleg_err, "g.--", label=raw"gauss-legendre quadrature rule")
+ax.set(xlabel=raw"grid size", ylabel=raw"error in quadrature")
+ax.set_title(raw"Quadrature convergence")
+ax.grid(true); ax.legend()
+plt.savefig("jprogram6.png")
