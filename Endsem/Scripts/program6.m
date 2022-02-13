62fe18be8d11f217c7263d34a138b683f9b91832
Dayalan Saravanan <dayalsaravanan@gmail.com>
2022-02-13 22:13:19 +0530

    gauss-legendre quadrature rule

--- Endsem/Scripts/program6.m
+++ Endsem/Scripts/program6.m
@@ -5,20 +5,25 @@
 
 """ Script to implement quadrature methods and see how the eroor behaves """
 
+# function to be integrated
 f = @(x) exp(-x^2)
 
+# exact value of the integral
 exact = 1.49364826562485405080
 
+# number of grid points
 N = [2, 5, 10, 20, 50, 100]
 
+# number of different set of grids
 Ngrids = length(N)
 
-gleg = zeros(Ngrids);
+gleg = zeros(Ngrids);   # gauss-legendre quadrature rule 
 
 for N = [2, 5, 10, 20, 50, 100]
     [xnode, wnode] = GaussLegendre(N);
     gleg = sum(wnode.*f(xnode));
 end
 
-glegerr = gleg - exact
+# error calculations
+gleg_err = abs(gleg - exact)
 fprintf(glegerr)
