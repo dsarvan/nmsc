3d7b47a1bf5aaf2f9d4c98fa616ba1b40183ab5a
D.Saravanan <dayalsaravanan@gmail.com>
2022-03-14 23:21:10 +0530

    euler explicit scheme for y' = 10y with y(0) = 1

--- Scripts/Octave/Euler/eulermethod.m
+++ Scripts/Octave/Euler/eulermethod.m
@@ -3,24 +3,23 @@
 # Name: D.Saravanan
 # Date: 02/12/2021
 
-""" Script to implement the Euler explicit scheme for y' = -10y with y(0) = 1 """
+""" Script to implement the Euler explicit scheme for y' = -10y with y(1) = 1 """
 
-tf = 10;
-dt = 0.01;
-N = tf/dt;
-t = zeros(N+1, 1);
-y = zeros(N+1, 1);
-t(1) = 0;
-y(1) = 1;
+td = 10;                % time period
+dt = 0.01;              % time step
+N = td/dt;              % number of steps
+t = zeros(N+1, 1);      % time step vector
+y = zeros(N+1, 1);      % solution vector
+t(1) = 0;              
+y(1) = 1;               
 
 for k = 1:N
     t(k+1) = t(k) + dt;
     y(k+1) = y(k) + dt * (-10 * y(k));
 end
 
-exact = exp(-10*t);
+exact = exp(-10*t);     % exact solution
 
-figure;
 plot(t, y, 'r+');
 hold on;
 plot(t, exact, 'k-');
