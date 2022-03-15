9c3ba7009322aa9b32f770a2e9ec51408a7287b0
D.Saravanan <dayalsaravanan@gmail.com>
2022-03-15 18:59:19 +0530

    euler explicit scheme for y' = -10y with y(1) = 1

--- Scripts/Octave/Euler/eulerexplicit.m
+++ Scripts/Octave/Euler/eulerexplicit.m
@@ -5,11 +5,11 @@
 
 """ Script to implement the Euler explicit scheme for y' = -10y with y(1) = 1 """
 
-t_ = 10;                % time period
-dt = 0.01;              % time step
-N = t_/dt;              % number of steps
-t = zeros(N+1, 1);      % time step vector
-y = zeros(N+1, 1);      % solution vector
+t_ = 10;                # time period
+dt = 0.02;              # time step
+N = t_/dt;              # number of steps
+t = zeros(N+1, 1);      # time vector
+y = zeros(N+1, 1);      # solution vector of euler explicit
 t(1) = 0;              
 y(1) = 1;               
 
@@ -18,9 +18,12 @@ for k = 1:N
     y(k+1) = y(k) + dt * (-10 * y(k));
 end
 
-exact = exp(-10*t);     % exact solution
+exact = exp(-10*t);     # exact solution
 
-plot(t, y, 'r+');
-hold on;
-plot(t, exact, 'k-');
-print('eulerexplicit.png', '-dpng')
+figure;  plot(t, y, 'r+');
+hold on; plot(t, exact, 'k-');
+xlabel('t'); ylabel('y(t)');
+legend('euler explicit', 'exact solution');
+set(legend, 'location', 'northeast');
+set(gca, 'title', text('string', 'Euler explicit scheme'));
+grid on; print('eulerexplicit.png', '-dpng')
