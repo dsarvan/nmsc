869975660e67807be597b3a117500848a4960b78
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-03-29 00:02:53 +0530

    script to implement the different quadrature and see how the error behaves

--- Endsem/Scripts/program6.py
+++ Endsem/Scripts/program6.py
@@ -5,14 +5,14 @@
 
 """ Script to implement quadrature methods and see how the error behaves """
 
-import matplotlib
 import matplotlib.pyplot as plt
-plt.style.use('classic')
 from matplotlib.ticker import ScalarFormatter
 import numpy as np
 
-matplotlib.rcParams["pgf.texsystem"] = "pdflatex"
-matplotlib.rcParams.update(
+plt.style.use("classic")
+plt.rcParams["text.usetex"] = True
+plt.rcParams["pgf.texsystem"] = "pdflatex"
+plt.rcParams.update(
     {
         "font.family": "serif",
         "font.size": 10,
@@ -21,8 +21,6 @@ matplotlib.rcParams.update(
         "figure.titlesize": 12,
     }
 )
-matplotlib.rcParams["text.usetex"] = True
-matplotlib.use("Agg")
 
 # end points of interval
 a, b = -1, 1
@@ -82,14 +80,14 @@ formatter = ScalarFormatter()
 formatter.set_scientific(False)
 
 fig, ax = plt.subplots()
-ax.plot(n, trap_err, "b.--", label=r"trapezoidal rule")
-ax.plot(n, tend_err, "r.--", label=r"trapezoidal rule 1st derivative")
-ax.plot(n, tent_err, "m.--", label=r"trapezoidal 1st \& 3rd derivative")
-ax.plot(n, gleg_err, "g.--", label=r"gauss-legendre quadrature rule")
+ax.plot(n, trap_err, "b.--", label=r"$trapezoidal\ rule$")
+ax.plot(n, tend_err, "r.--", label=r"$trapezoidal\ rule\ 1st\ derivative$")
+ax.plot(n, tent_err, "m.--", label=r"$trapezoidal\ 1st\ \&\ 3rd\ derivative$")
+ax.plot(n, gleg_err, "g.--", label=r"$gauss-legendre\ quadrature\ rule$")
 ax.set_xscale("log", base=2)
 ax.set_yscale("log", base=10)
 ax.xaxis.set_major_formatter(formatter)
-ax.set(xlabel=r"number of grid points", ylabel=r"error in quadrature")
-ax.set_title(r"Quadrature convergence")
+ax.set(xlabel=r"$grid\ points$", ylabel=r"$quadrature\ error$")
+ax.set_title(r"$Quadrature\ convergence$")
 ax.grid(True); ax.legend(loc="lower left")
 plt.savefig("program6.pgf")
