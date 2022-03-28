39ab99bd2c6abc17e35216812533abfc84bc7b4f
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-03-28 19:52:18 +0530

    script to compute the angular displacement and angular velocity for a simple pendulum using euler's method

--- Endsem/Scripts/Program8/program8.py
+++ Endsem/Scripts/Program8/program8.py
@@ -6,8 +6,8 @@
 """ Script to compute the angular displacement and angular velocity for a simple
 pendulum using Euler's method """
 
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.style.use("classic")
 plt.rcParams["text.usetex"] = True
@@ -22,31 +22,56 @@ plt.rcParams.update(
     }
 )
 
-t_ = 100                    # time period
+t_ = 100  # time period (s)
 
-for n, dt in enumerate([0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20]):   # time step
-    
-    N = int(t_/dt)          # number of steps
-    print(n, dt, t_, N)
-    t = np.zeros(N+1)       # time vector
+for n, dt in enumerate([0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20]):  # time step
 
-    theta = np.ones(N+1)    # angular displacement
-    omega = np.ones(N+1)    # angular velocity
+    N = int(t_ / dt)  # number of steps
+    t = np.zeros(N + 1)  # time vector (s)
 
-    theta[0] = np.pi/6      # initial value of angular displacement
-    omega[0] = 0.0          # initial value of angular velocity
+    theta1 = np.ones(N + 1)  # angular displacement
+    omega1 = np.ones(N + 1)  # angular velocity
+
+    theta1[0] = np.pi / 6  # initial value of angular displacement (rad)
+    omega1[0] = 0.0  # initial value of angular velocity (1/s)
+
+    theta2 = np.ones(N + 1)  # angular displacement
+    omega2 = np.ones(N + 1)  # angular velocity
+
+    theta2[0] = np.pi / 6  # initial value of angular displacement (rad)
+    omega2[0] = 0.0  # initial value of angular velocity (1/s)
+
+    theta3 = np.ones(N + 1)  # angular displacement
+    omega3 = np.ones(N + 1)  # angular velocity
+
+    theta3[0] = np.pi / 6  # initial value of angular displacement (rad)
+    omega3[0] = 0.0  # initial value of angular velocity (1/s)
 
     for k in range(0, N):
 
-        t[k+1] = t[k] + dt
+        t[k + 1] = t[k] + dt
 
         # forward euler
-        theta[k+1] = theta[k] + omega[k] * dt
-        omega[k+1] = omega[k] - np.sin(theta[k]) * dt
+        theta1[k + 1] = theta1[k] + omega1[k] * dt
+        omega1[k + 1] = omega1[k] - np.sin(theta1[k]) * dt
+
+        # backward euler
+        theta2[k + 1] = theta2[k] + (omega2[k] - np.sin(theta2[k]) * dt) * dt
+        omega2[k + 1] = omega2[k] - np.sin(theta2[k] + omega2[k] * dt) * dt
+
+        # trapezoidal scheme
+        theta3[k + 1] = theta3[k] + (dt / 2) * (
+            omega3[k] + omega3[k] - np.sin(theta3[k]) * dt
+        )
+        omega3[k + 1] = omega3[k] - (dt / 2) * (
+            np.sin(theta3[k]) + np.sin(theta3[k] + dt * omega3[k])
+        )
 
     figure, ax = plt.subplots()
-    ax.plot(t, theta, "r--", label="{}".format(dt))
+    ax.plot(t, theta1, "r-", label=r"$euler\ explicit$")
+    ax.plot(t, theta2, "b-", label=r"$euler\ implicit$")
+    ax.plot(t, theta3, "k-", label=r"$trapezoidal\ scheme$")
     ax.set(xlabel=r"$time\ (s)$", ylabel=r"$\theta\ (rad)$")
-    ax.set_title(r"$Simple\ pendulum\ using\ Euler's\ methods\ (time\ step = {}\ (s))$".format(dt)) 
-    ax.grid(True); ax.legend()
-    plt.savefig("program8{}.png".format(n)) 
+    ax.set_title(r"$Simple\ pendulum\ using\ Euler's\ methods\ (time\ step = {}\ (s))$".format(dt))
+    ax.grid(True); ax.legend(loc="best")
+    plt.savefig("program8{}.png".format(n))
