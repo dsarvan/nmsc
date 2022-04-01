b9ea5dd51a9df5c63a6ef72f1159d09f9fdc11f7
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-04-01 20:32:32 +0530

    computation of weights added

--- Scripts/Python/Gaussquad/chebyshevnodes.py
+++ Scripts/Python/Gaussquad/chebyshevnodes.py
@@ -3,7 +3,7 @@
 # Name: D.Saravanan
 # Date: 08/03/2022
 
-""" Script to compute the nodes of the Chebyshev polynomial of the first kind of degree n """
+""" Script to compute the nodes and weights of the Chebyshev polynomial of the first kind of degree n """
 
 import numpy as np
 
@@ -12,10 +12,14 @@ a = int(input("lower limit: "))
 b = int(input("upper limit: "))
 
 x = np.zeros(n+1)
+w = np.zeros(n+1)
 
-#xn, wn = np.polynomial.chebyshev.chebgauss(n)
-#print("nodes computed: {}\n".format(xn))
+xn, wn = np.polynomial.chebyshev.chebgauss(n)
+print("nodes computed: {}\n".format(xn))
+print("weights: {}\n".format(wn))
 
 for k in range(1, n+1):
     x[k] = 0.5*(b - a) * np.cos(((2*k-1)/(2*n))*np.pi) + 0.5*(b + a)
+    w[k] = np.pi/(n)
     print("x[{}] = {:.8f}".format(k, x[k]))
+    print("w[{}] = {:.8f}".format(k, w[k]))
