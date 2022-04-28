46027a3c71578b4d5fd34b4301d6251d75724682
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-04-28 16:01:43 +0530

    script to calculate nodes and weights of gauss-legendre quadrature

--- Scripts/Julia/Gaussquad/gaussleg.jl
+++ Scripts/Julia/Gaussquad/gaussleg.jl
@@ -4,3 +4,19 @@
 # Date: 08/03/2022
 
 """ Script to calculate nodes and weights of Gauss-Legendre quadrature """
+
+using FastGaussQuadrature, LinearAlgebra
+
+# exact value of the integral
+f = x -> sin(x**2)
+
+# nodes and weights calculations
+x, w = gausslegendre(10)
+
+# integration
+integral = w * sin(x**2)
+
+# error calculation
+error = abs(integral - exact)
+
+println(exact, integral, error)
