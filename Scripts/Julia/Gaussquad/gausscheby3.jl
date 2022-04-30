6c9bb5e87b7047b627654f6ba5b321138b077d4d
D.Saravanan <dayalsaravanan@gmail.com>
2022-04-30 22:14:26 +0530

    script to calculate nodes and weights of gauss-chebyshev quadrature of the 3rd kind

---
+++ Scripts/Julia/Gaussquad/gausscheby3.jl
@@ -0,0 +1,24 @@
+#!/usr/bin/env julia
+# File: gausscheby3.jl
+# Name: D.Saravanan
+# Date: 08/03/2022
+
+""" Script to calculate nodes and weights of Gauss-Chebyshev quadrature of the 3rd kind """
+
+using SciPy
+using FastGaussQuadrature, LinearAlgebra
+
+# exact value of the integral
+f = x -> (x^4)/sqrt(1 - x^2)
+exact = SciPy.integrate.quad(f, -1, 1)[1]
+
+# nodes and weights calculations
+x, w = gausschebyshev(10, 3)
+
+# integration
+integral = w' * x.^4
+
+# error calculation
+error = abs(integral - exact)
+
+println(exact, ' ', integral, ' ', error)
