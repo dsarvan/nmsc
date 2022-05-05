6cd007d2d7f1343f2c684010b2b79eb93c5ccb06
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-05-05 23:38:16 +0530

    create solution vectors for euler explicit, implicit and trapezoidal rule

--- Scripts/Julia/Euler/eulerscheme.jl
+++ Scripts/Julia/Euler/eulerscheme.jl
@@ -1,5 +1,14 @@
+#!/usr/bin/env julia
 # File: eulerscheme.jl
 # Name: D.Saravanan
 # Date: 02/12/2021
 
 """ Script to implement the Euler explicit, implicit and Trapezoidal scheme for y' = -0.5*y with y(0) = 1 """
+
+t_ = 25                 # time period
+dt = 0.5                # time step
+N = int(t_/dt)          # number of steps
+t = zeros(N)            # time vector
+y1 = zeros(N)           # solution vector of euler explicit
+y2 = zeros(N)           # solution vector of euler implicit
+y4 = zeros(N)           # solution vector of trapezoidal rule
