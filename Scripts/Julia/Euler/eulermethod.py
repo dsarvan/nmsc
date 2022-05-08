8df2a2d3f069488f8611336893d288655efbc56a
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-05-08 21:51:55 +0530

    euler explicit & implicit scheme for y' = -0.5y with y(0) = 1

---
+++ Scripts/Julia/Euler/eulermethod.py
@@ -0,0 +1,40 @@
+!#/usr/bin/env julia
+# File: eulermethod.jl
+# Name: D.Saravanan
+# Date: 02/12/2021
+
+""" Script to implement the Euler explicit & implicit scheme for y' = -0.5*y with y(0) = 1 """
+
+import PyPlot
+const plt = PyPlot
+plt.rc("pgf", texsystem="pdflatex")
+plt.rc("font", family="serif", weight="normal", size=8)
+plt.rc("axes", labelsize=10, titlesize=10)
+plt.rc("figure", titlesize=10)
+plt.rc("text", usetex="True")
+using LaTeXStrings
+
+t_ = 25                 # time period
+dt = 0.5                # time step
+N = int(t_/dt)          # number of steps
+t = zeros(N)            # time vector
+y1 = zeros(N)           # solution vector of euler explicit
+y2 = zeros(N)           # solution vector of euler implicit
+
+for k in 1:N
+    t[k+1] = t[k] + dt
+    y1[k+1] = y1[k]*(1 - 0.5*dt)
+    y2[k+1] = y2[k]/(1 + 0.5*dt)
+end
+
+exact = exp(-0.5*t)     # exact solution
+
+fig, ax = plt.subplots()
+ax.plot(t, y1, "r.", label=raw"euler explicit")
+ax.plot(t, y2, "b.", label=raw"euler implicit")
+ax.plot(t, y3, "g.", label=raw"trapezoidal rule")
+ax.plot(t, exact, "k-", label=raw"exact solution")
+ax.set(xlabel=raw"t", ylabel=raw"y(t)")
+ax.set_title(raw"Euler explicit \& implicit scheme")
+ax.grid(true); ax.legend()
+plt.savefig("eulermethod.pdf")
