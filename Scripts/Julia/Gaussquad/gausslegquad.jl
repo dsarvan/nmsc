9f26d19644d368f0a61b1644f08bbba4af472729
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-05-26 23:50:20 +0530

    script to evaluate integral exp(-x^2) from -1 to +1

---
+++ Scripts/Julia/Gaussquad/gausslegquad.jl
@@ -0,0 +1,24 @@
+#!/usr/bin/env julia
+# File: gausslegquad.jl
+# Name: D.Saravanan
+# Date: 08/03/2022
+
+""" Script to evaluate integral exp(-x^2) from -1 to +1 """
+
+using SciPy
+using FastGaussQuadrature, LinearAlgebra
+
+# exact value of the integral
+f = x -> exp(-x^2)
+exact = SciPy.integrate.quad(f, -1, 1)[1]
+
+for k in [2, 5, 10, 20, 50, 100]
+    
+    # nodes and weights calculations
+    x, w = gausslegendre(k)
+
+    # integration
+    integral = (w' * f.(x))
+
+    # error calculation
+    error = abs(integral - exact)
