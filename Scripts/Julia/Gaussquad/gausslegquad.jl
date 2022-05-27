fc2434c027c710868c8c9202776ab01efcaae14a
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-05-27 23:49:33 +0530

    script to evaluate integral exp(-x**2) from -1 to +1

--- Scripts/Julia/Gaussquad/gausslegquad.jl
+++ Scripts/Julia/Gaussquad/gausslegquad.jl
@@ -12,7 +12,11 @@ using FastGaussQuadrature, LinearAlgebra
 f = x -> exp(-x^2)
 exact = SciPy.integrate.quad(f, -1, 1)[1]
 
+n = []
+
 for k in [2, 5, 10, 20, 50, 100]
+
+    n.append(k)
     
     # nodes and weights calculations
     x, w = gausslegendre(k)
