779964854242716879feaeb3e4171461338b0094
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-06-07 18:56:36 +0530

    script to compute the nodes and weights of the chebyshev polynomial of the first kind of degree n

--- Scripts/Julia/Gaussquad/chebyshevnodes.jl
+++ Scripts/Julia/Gaussquad/chebyshevnodes.jl
@@ -11,3 +11,5 @@ using FastGaussQuadrature, LinearAlgebra
 for k in 1:n
     x[k] = 0.5*(b - a) * cos(((2*k-1)/(2*n))*pi) + 0.5*(b + a)
     w[k] = pi/(n)
+    println(k, x[k])
+    println(k, w[k])
