779da4ed3dfe9d1c94d856902d3a3464a8e4a934
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-06-25 23:56:06 +0530

    script to solve the differential equation

--- Scripts/Julia/Odeint/problem.jl
+++ Scripts/Julia/Odeint/problem.jl
@@ -5,11 +5,9 @@
 
 """ Script to solve the differential equation """
 
-using DifferrentialEquations
+using DifferentialEquations
 
-function f(y, t)
-    return -0.5*y
-end
+f(y, p, t) = -0.5y
 
 # initial condition
 y0 = 1
@@ -19,6 +17,3 @@ t = (0, 25)
 
 # solve ODE
 y = ODEProblem(f, y0, t)
-
-# exact solution
-exact = exp(-0.5*t)
